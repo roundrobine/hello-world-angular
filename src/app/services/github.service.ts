08519fce64e8796b71e46ed0ea71f245f5c49c69
Dimitar <roundrobine@gmail.com>
2020-04-12 12:25:24 +0200

    remove inject annotation from services

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { DataService } from "./data.service";
-@Injectable({
-  providedIn: "root"
-})
+
 export class GithubService extends DataService {
-  constructor(http: HttpClient) {
-    super("https://api.github.com/users/roundrobine/following", http);
-  }
+    constructor(http: HttpClient) {
+        super("https://api.github.com/users/roundrobine/following", http);
+    }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,11 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { DataService } from "./data.service";
-@Injectable({
-  providedIn: "root"
-})
+
 export class PostService extends DataService {
-  constructor(http: HttpClient) {
-    super("https://jsonplaceholder.typicode.com/posts", http);
-  }
+    constructor(http: HttpClient) {
+        super("https://jsonplaceholder.typicode.com/posts", http);
+    }
 }
