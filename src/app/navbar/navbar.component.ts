56522ea00e974ef12625eb53dafa838835b9a31e
Dimitar <roundrobine@gmail.com>
2020-04-12 13:20:08 +0200

    use env config to show different navbar color for dev and prod

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
+import { environment } from "./../../environments/environment";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "navbar",
-  templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.css"]
+    selector: "navbar",
+    templateUrl: "./navbar.component.html",
+    styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+    navBarBackgroundColor = environment.navBarBackgroundColor;
 
-  ngOnInit() {}
+    constructor() {}
+
+    ngOnInit() {}
 }
